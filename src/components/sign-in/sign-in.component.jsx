554952b157c5e5a5af80e2788b8d9505005afa06
Yonatan Snir <yonatan941@gmail.com>
2019-11-21 11:46:09 +0200

    new update

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -50,10 +50,12 @@ class SignIn extends React.Component {
             required
           />
 
-          <CustomButton type="submit">Sign in</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            Sign in with Google
-          </CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit">Sign in</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div> 
         </form>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Switch, Route } from "react-router-dom";
@@ -9,19 +9,42 @@ import Header from "./components/header/header.component";
 import SignInAndUp from "./pages/sign-in-up/sign-in-up.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndUp} />
-      </Switch>
-    </div>
-  );
-}
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubcribeFromAuth = null;
+
+  componentDidMount(){
+    this.unsubcribeFromAuth =   auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubcribeFromAuth();
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndUp} />
+        </Switch>
+      </div>
+    );
+}}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
